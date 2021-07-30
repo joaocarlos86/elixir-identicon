eb669c96e0030fbe27a3a289bf5b4909b77d7eac
Joao Rodrigues <jrodrigues@overstock.com>
2021-07-30 15:48:31 +0100

    Using pattern matching with structs to get the first three elements from the array inside the structs and the rest of the list (_tail)

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -19,6 +19,12 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [red, green, blue | _tail]} = image
+    [red, green, blue]
   end
 
   def hash_input(input) do
